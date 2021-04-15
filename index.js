57cfb7a41c9ff9263de45792fc9aececdcf15078
Courtney Brown <courtneybrown@Courtneys-MacBook-Pro.local>
2021-04-15 11:41:47 -0500

    manager, team, engineer, and intern prompts are working

--- index.js
+++ index.js
@@ -35,9 +35,103 @@ Team.prototype.promptManager = function () {
     .then(({ name, id, email, officeNumber }) => {
         this.manager = new Manager(name, id, email, officeNumber);
         console.log(this.manager);
+
+        this.promptTeam()
+        }) 
+};
+
+Team.prototype.promptTeam = function (teamArray) {
+
+    // if (!teamArray.members) {
+    //     teamArray.members = [];
+    // }
+    return inquirer
+        .prompt([
+        {
+            type: "list",
+            name: "add",
+            message: "Which type of team member would you like to add?",
+            choices: ["Engineer", "Intern", "I'm finished building my team."]
+        }
+    ])
+    .then(teamArray => {
+        console.log(teamArray.add)
+
+        if(teamArray.add === 'Engineer') {
+            console.log(teamArray.add)
+            return this.promptEngineer(teamArray);
+        } else if (teamArray.add === 'Intern') {
+            return this.promptIntern(teamArray);
+        } else {
+            return teamArray;
+        }
+    });
+};
+
+Team.prototype.promptEngineer = function(teamArray) {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your engineer's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your engineer's id?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your engineer's email?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your engineer's github username?"
+        },
+    ])
+    .then(({ name, id, email, github }) => {
+        this.engineer = new Engineer(name, id, email, github);
+        console.log(this.engineer);
+    }) 
+    .then(teamMember => {
+        teamArray.members.push(teamMember);
+        return promptTeam(teamArray);
     })
+};
 
-    
-}
+Team.prototype.promptIntern = function(teamArray) {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your intern's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your intern's id?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your intern's email?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your intern's school?"
+        },
+    ])
+    .then(({ name, id, email, school }) => {
+        this.intern = new Intern(name, id, email, school);
+        console.log(this.intern);
+    })
+    .then(teamMember => {
+        teamArray.members.push(teamMember);
+        return promptTeam(teamArray);
+    }) 
+};
 
-new Team().promptManager();
+new Team().promptManager()
