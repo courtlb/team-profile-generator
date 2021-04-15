4790ffffb8b0d8b4da24612007da615a292f2669
Courtney Brown <courtneybrown@Courtneys-MacBook-Pro.local>
2021-04-15 10:17:31 -0500

    two failing intern tests

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,20 @@
+const Intern = require('../lib/Intern');
+
+test('creates intern object', () => {
+    const intern = new Intern('Courtney', 1, 'email', 'texas');
+
+    expect(intern.name).toBe('Courtney');
+    expect(intern.id).toBe(1);
+    expect(intern.email).toBe('email');
+    expect(intern.github).toBe('texas');
+});
+
+test('gets intern information', () => {
+    const intern = new Intern ('Courtney', 1, 'email', 'texas');
+
+    expect(intern.getName()).toBe('Courtney');
+    expect(intern.getId()).toBe(1);
+    expect(intern.getEmail()).toBe('email');
+    expect(intern.getGithub()).toBe('texas');
+    expect(intern.getRole()).toBe('intern');
+});
