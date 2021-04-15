8a9e728846ddb669907f727d7a03488f33b2866b
Courtney Brown <courtneybrown@Courtneys-MacBook-Pro.local>
2021-04-15 10:12:23 -0500

    both manager tests passing

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,3 +1,4 @@
+const { expect } = require('@jest/globals');
 const Manager = require('../lib/Manager');
 
 test('creates manager object', () => {
@@ -7,4 +8,14 @@ test('creates manager object', () => {
     expect(manager.id).toBe(1);
     expect(manager.email).toBe('email');
     expect(manager.officeNumber).toBe(100);
+});
+
+test('gets manager information', () => {
+    const manager = new Manager ('Courtney', 1, 'email', 100);
+
+    expect(manager.getName()).toBe('Courtney');
+    expect(manager.getId()).toBe(1);
+    expect(manager.getEmail()).toBe('email');
+    expect(manager.getOfficeNumber()).toBe(100);
+    expect(manager.getRole()).toBe('manager');
 });
